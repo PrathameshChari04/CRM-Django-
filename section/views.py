981327f87c6323ee1168fb9041bd940c6de0da82
PrathameshChari04 <pc939791@gmail.com>
2020-05-13 22:39:57 +0530

    first commit

---
+++ section/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+
+def index(request):
+
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'customers':customers, 'orders':orders, 'total_customers':total_customers,
+    'total_orders':total_orders,'delivered':delivered,'pending':pending}
+    return render(request,'section/dashboard.html',context)
+
+def products(request):
+
+    products = Product.objects.all()
+    return render(request,'section/products.html',{'products':products})
+
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {'customer':customer,'orders':orders,'order_count' : order_count,}
+    return render(request,'section/customer.html',context)
+
+    
